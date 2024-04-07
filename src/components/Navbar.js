9dcc31154a57b1a3f252b20f9d3b3e744182dbc6
Abdullah-tgg <116303690+Abdullah-tgg@users.noreply.github.com>
2024-04-07 12:56:11 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/">HomeUpdated</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/">Contact</Link>
